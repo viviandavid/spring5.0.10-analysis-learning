1df3d159a542f71f0ce845612296b81f5bda2042
lsd <viviandavid@126.com>
2019-12-09 20:19:53 +0800

    commit

---
+++ spring-framework-5.0.10.RELEASE/spring-analyze-note/src/main/java/LSD/AppConfig.java
@@ -0,0 +1,10 @@
+package LSD;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("LSD")
+public class AppConfig {
+}
---
+++ spring-framework-5.0.10.RELEASE/spring-analyze-note/src/main/java/LSD/DemoDao.java
@@ -0,0 +1,12 @@
+package LSD;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class DemoDao {
+	public void query() {
+		System.out.println("hello spring code!");
+	}
+
+}
